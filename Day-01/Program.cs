7701915e51141674dff7df9b2749a17b20345940
Noah O'Brien <nobrien@iprotech.com>
2022-12-06 15:33:02 -0700

    Day 1 Advent of Code

---
+++ Day-01/Program.cs
@@ -0,0 +1,21 @@
+﻿var sum = 0;
+var maxSum = 0;
+
+foreach(string line in System.IO.File.ReadLines(@"./day-01-input.txt"))
+{
+    if(!String.IsNullOrEmpty(line))
+    {
+        sum += Int32.Parse(line);
+        continue;
+    }
+
+    if(sum > maxSum)
+        maxSum = sum;
+
+    sum = 0;
+}
+
+if(sum > maxSum)
+   maxSum = sum;
+
+Console.WriteLine(maxSum);
