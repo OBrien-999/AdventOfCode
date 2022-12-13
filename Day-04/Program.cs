15aa5b134c77ef370c745f61eef012ab7b5cc48c
Veronica Villar <vvillar@ipro.com>
2022-12-13 15:22:34 -0800

    Fix bug

--- Day-04/Program.cs
+++ Day-04/Program.cs
@@ -13,22 +13,18 @@ foreach(string sectionPair in System.IO.File.ReadLines(@"./day-04-input.txt"))
     var startPairTwo = Int32.Parse(matches[0].Groups[3].Value);
     var endPairTwo = Int32.Parse(matches[0].Groups[4].Value);
 
-    if(startPairOne < startPairTwo)
-    {
-        IEnumerable<int> widerRange = Enumerable.Range(startPairOne, endPairOne);
-        var doesRangeContainOther = widerRange.Contains(startPairTwo) && widerRange.Contains(endPairTwo);
-        totalContainedPairs += doesRangeContainOther ? 1 : 0;
-    }
-    else if(startPairTwo < startPairOne)
-    {
-        IEnumerable<int> widerRange = Enumerable.Range(startPairTwo, endPairTwo);
-        var doesRangeContainOther = widerRange.Contains(startPairOne) && widerRange.Contains(endPairOne);
-        totalContainedPairs += doesRangeContainOther ? 1 : 0;
-    }
+    if (startPairOne < startPairTwo)
+        totalContainedPairs += DoesRangeContainOther(startPairOne, endPairOne, startPairTwo, endPairTwo) ? 1 : 0;
+    else if (startPairTwo < startPairOne)
+        totalContainedPairs += DoesRangeContainOther(startPairTwo, endPairTwo, startPairOne, endPairOne) ? 1 : 0;
     else
-    {
         totalContainedPairs += 1;
-    }
 }
 
 Console.Write(totalContainedPairs);
+
+static bool DoesRangeContainOther(int widerRangeStart, int widerRangeEnd, int shorterRangeStart, int shorterRangeEnd)
+{
+    IEnumerable<int> widerRange = Enumerable.Range(widerRangeStart, widerRangeEnd - widerRangeStart + 1);
+    return widerRange.Contains(shorterRangeStart) && widerRange.Contains(shorterRangeEnd);
+}
