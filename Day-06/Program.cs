2d6f811b7a37235602e15e6c0a343689a62e3c11
Noah O'Brien <nobrien@iprotech.com>
2022-12-21 15:44:26 -0700

    Complete day 6 (both part 1 and 2)

---
+++ Day-06/Program.cs
@@ -0,0 +1,24 @@
+﻿var signal = System.IO.File.ReadAllText(@"./day-06-input.txt");
+var packetSize = args.Count() > 0 ?  Int32.Parse(args[0]) : 4;
+
+var dataLength = signal.Length;
+var startCharacterPosition = 0;
+var endCharacterPosition = 0;
+
+HashSet<char> packet = new HashSet<char>();
+for(; endCharacterPosition < dataLength; endCharacterPosition++)
+{
+    if(!packet.Add(signal[endCharacterPosition]))
+    {
+        startCharacterPosition++;
+        endCharacterPosition = (startCharacterPosition - 1);
+
+        packet.Clear();
+        continue;
+    }
+  
+    if((endCharacterPosition - startCharacterPosition + 1) == packetSize)
+        break;
+}
+
+Console.WriteLine(endCharacterPosition + 1);
