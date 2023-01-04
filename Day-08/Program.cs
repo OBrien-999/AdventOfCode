8911189742324d71ad8dbf9bfb9ee879a4e8eafa
Noah O'Brien <nobrien@iprotech.com>
2023-01-04 16:48:09 -0700

    Finished day 8 part 2

--- Day-08/Program.cs
+++ Day-08/Program.cs
@@ -2,9 +2,8 @@
 
 string[] lines = System.IO.File.ReadAllLines(@"./day-08-input.txt");
 var grid = new Dictionary<string, Tree>();
-
-var maxY = lines.Length - 1;
-var maxX = lines[0].Length - 1;
+int MAX_X = lines[0].Length - 1;
+int MAX_Y = lines.Length - 1;
 
 var row = 0;
 var column = 0;
@@ -15,9 +14,9 @@ foreach (var line in lines)
     {
         var tree = new Tree(column, row, Int32.Parse(number.ToString())) {
             IsVisibleFromLeftEdge = column == 0,
-            IsVisibleFromRightEdge = column == maxX,
+            IsVisibleFromRightEdge = column == MAX_X,
             IsVisibleFromTopEdge = row == 0,
-            IsVisibleFromBottomEdge = row == maxY
+            IsVisibleFromBottomEdge = row == MAX_Y
         };
         grid.Add(tree.ToString(), tree);
         column++;
@@ -28,11 +27,13 @@ foreach (var line in lines)
 
 int recursiveTreeCheckCount = 0; // To compare calls with/without optimizations
 
-findVisibleTrees(grid, startX: 0, startY: 0, endX: maxX, endY: maxY);
+findVisibleTrees(grid, startX: 0, startY: 0, endX: MAX_X, endY: MAX_Y);
 
 var visibleTreeCount = grid.Select(x => x.Value).Where(t => t.IsVisible).Count();
+var highestScenicScore = grid.Select(x => x.Value.ScenicScore).Max();
 
-Console.WriteLine(visibleTreeCount);
+Console.WriteLine($"Visible trees from outside the grid: {visibleTreeCount}");
+Console.WriteLine($"Highest scenic score: {highestScenicScore}");
 // Console.WriteLine(recursiveTreeCheckCount); 
 
 void findVisibleTrees(Dictionary<string, Tree> grid, int startX, int startY, int endX, int endY)
@@ -81,47 +82,68 @@ bool CheckTreeVisibility(Tree rootTree, int nextX, int nextY, string direction)
     
     var neighborTree = grid.GetValueOrDefault($"{nextX},{nextY}");
     var rootLargerThanNextNeighbor = rootTree.Value > neighborTree.Value;
-    if (!rootLargerThanNextNeighbor)
+    var rootEqualToNextNeighbor = rootTree.Value == neighborTree.Value;
+
+    if (!rootLargerThanNextNeighbor || rootEqualToNextNeighbor)
+    {
+        switch(direction)
+        {
+            case "left":
+                rootTree.ViewingDistanceLeft = rootTree.X - neighborTree.X;
+                break;
+            case "right":
+                rootTree.ViewingDistanceRight = neighborTree.X - rootTree.X;
+                break;
+            case "up":
+                rootTree.ViewingDistanceTop = rootTree.Y - neighborTree.Y;
+                break;
+            case "down":
+                rootTree.ViewingDistanceBottom = neighborTree.Y - rootTree.Y;
+                break;
+        }
+
         return false;
-    
-    
-    if (direction == "left") {
-        if (neighborTree.X == 0) // Neighbor is at left edge
-            return rootLargerThanNextNeighbor;
-
-        if (neighborTree.IsVisibleFromLeftEdge)
-            return true;
-
-        return CheckTreeVisibility(rootTree, neighborTree.X - 1, rootTree.Y, "left");
-        
-    } else if (direction == "right") {
-        if (neighborTree.X == maxX) // Neighbor is at right edge
-            return rootLargerThanNextNeighbor;
-
-        if (neighborTree.IsVisibleFromRightEdge)
-            return true;
-
-        return CheckTreeVisibility(rootTree, neighborTree.X + 1, rootTree.Y, "right");
-        
-    } else if (direction == "up") {
-        if (neighborTree.Y == 0) // Neighbor is at top edge
-            return rootLargerThanNextNeighbor;
-
-        if (neighborTree.IsVisibleFromTopEdge)
-            return true;
-
-        return CheckTreeVisibility(rootTree, rootTree.X, neighborTree.Y - 1, "up");
-
-    } else {
-        if (neighborTree.Y == maxY) // Neighbor is at top edge
-            return rootLargerThanNextNeighbor;
-
-        if (neighborTree.IsVisibleFromBottomEdge)
-            return true;
-                
-        return CheckTreeVisibility(rootTree, rootTree.X, neighborTree.Y + 1, "down");
     }
 
+    switch(direction)
+    {
+        case "left":
+            if (neighborTree.X == 0 || neighborTree.IsVisibleFromLeftEdge) // Neighbor is at left edge
+            { 
+                rootTree.ViewingDistanceLeft = rootTree.X;
+                return true;
+            }
+
+            return CheckTreeVisibility(rootTree, neighborTree.X - 1, rootTree.Y, "left");
+
+        case "right":
+            if (neighborTree.X == MAX_X || neighborTree.IsVisibleFromRightEdge) // Neighbor is at right edge
+            {
+                rootTree.ViewingDistanceRight = MAX_X - rootTree.X;
+                return true;
+            }
+
+            return CheckTreeVisibility(rootTree, neighborTree.X + 1, rootTree.Y, "right");
+
+        case "up":
+            if (neighborTree.Y == 0 || neighborTree.IsVisibleFromTopEdge) // Neighbor is at top edge
+            {
+                rootTree.ViewingDistanceTop = rootTree.Y;
+                return true;
+            }
+
+            return CheckTreeVisibility(rootTree, rootTree.X, neighborTree.Y - 1, "up");
+
+        default:
+            if (neighborTree.Y == MAX_Y || neighborTree.IsVisibleFromBottomEdge) // Neighbor is at top edge
+            {
+                rootTree.ViewingDistanceBottom = MAX_Y - rootTree.Y;
+                return true;
+            }
+                    
+            return CheckTreeVisibility(rootTree, rootTree.X, neighborTree.Y + 1, "down");
+
+    }
 }
 
 public record class Tree 
@@ -129,6 +151,13 @@ public record class Tree
     public readonly int X;
     public readonly int Y;
     public readonly int Value;
+
+    public int ViewingDistanceLeft = 0;
+    public int ViewingDistanceRight = 0;
+    public int ViewingDistanceTop = 0;
+    public int ViewingDistanceBottom = 0;
+
+    public int ScenicScore => ViewingDistanceLeft * ViewingDistanceRight * ViewingDistanceTop * ViewingDistanceBottom;
     
     public bool IsVisibleFromRightEdge = false;
     public bool IsVisibleFromLeftEdge = false;
